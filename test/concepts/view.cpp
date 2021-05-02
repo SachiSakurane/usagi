e8746f32993bd67f540a62e278901f837a49b90f
SachiSakurane <sakuranesachi@gmail.com>
2021-05-02 18:37:07 +0900

    add test

--- test/concepts/view.cpp
+++ test/concepts/view.cpp
@@ -14,14 +14,16 @@ struct View
 struct MissViewConcepts
 {
   using value_type = float;
-  value_type width;
+  using affine_type = usagi::affine<value_type>;
+  decltype(auto) width() const { return value_type{}; }
+  decltype(auto) height() const { return value_type{}; }
 };
 
 bool ViweStaticTest()
 {
   static_assert(usagi::concepts::view<View<float>>, "ViewConcepts<float> has view concept");
   static_assert(!usagi::concepts::view<View<int>>, "ViewConcepts<int> hasn't view concept");
-
+  static_assert(!usagi::concepts::view<MissViewConcepts>, "MissViewConcepts hasn't view concept");
   return true;
 }
 

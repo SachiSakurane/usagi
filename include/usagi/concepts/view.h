df36e29c7b1398925e0c0ed225e8948e84779d6b
SachiSakurane <sakuranesachi@gmail.com>
2021-05-03 02:17:26 +0900

    wa

--- include/usagi/concepts/view.h
+++ include/usagi/concepts/view.h
@@ -10,7 +10,6 @@ namespace usagi::concepts
   template <class ViewType>
   concept view = requires()
   {
-    typename ViewType::value_type;
     typename ViewType::affine_type;
     {
       std::declval<std::add_const_t<ViewType>>().width()

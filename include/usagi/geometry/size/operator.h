a9b16f9430c0dfe39fd22f08d8feef302f1f4d81
SachiSakurane <sakuranesachi@gmail.com>
2021-06-21 09:51:20 +0900

    optimized size

--- include/usagi/geometry/size/operator.h
+++ include/usagi/geometry/size/operator.h
@@ -4,139 +4,133 @@
 
 namespace usagi
 {
-  template <class Type>
-  inline constexpr bool operator==(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr bool operator==(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
     return lhs.width() == rhs.width() && lhs.height() == rhs.height();
   }
 
-  template <class Type>
-  inline constexpr bool operator!=(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr bool operator!=(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
     return !(lhs == rhs);
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator+(typename usagi::size<Type>::value_type &&lhs, const usagi::size<Type> &rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator+(typename SizeType::value_type &&lhs, const SizeType &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs + rhs.width(); },
-        [lhs, rhs]()
-        { return lhs + rhs.height(); }};
+        { return std::make_tuple(lhs + rhs.width(), lhs + rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator+(const usagi::size<Type> &lhs, typename usagi::size<Type>::value_type &&rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator+(const SizeType &lhs, typename SizeType::value_type &&rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs.width() + rhs; },
-        [lhs, rhs]()
-        { return lhs.height() + rhs; }};
+        { return std::make_tuple(lhs.width() + rhs, lhs.height() + rhs); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator+(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr decltype(auto) operator+(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeTypeA::value_type>{
         [lhs, rhs]()
-        { return lhs.width() + rhs.width(); },
-        [lhs, rhs]()
-        { return lhs.height() + rhs.height(); }};
+        { return std::make_tuple(lhs.width() + rhs.width(), lhs.height() + rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator-(typename usagi::size<Type>::value_type &&lhs, const usagi::size<Type> &rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator-(typename SizeType::value_type &&lhs, const SizeType &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs - rhs.width(); },
-        [lhs, rhs]()
-        { return lhs - rhs.height(); }};
+        { return std::make_tuple(lhs - rhs.width(), lhs - rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator-(const usagi::size<Type> &lhs, typename usagi::size<Type>::value_type &&rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator-(const SizeType &lhs, typename SizeType::value_type &&rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs.width() - rhs; },
-        [lhs, rhs]()
-        { return lhs.height() - rhs; }};
+        { return std::make_tuple(lhs.width() - rhs, lhs.height() - rhs); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator-(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr decltype(auto) operator-(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeTypeA::value_type>{
         [lhs, rhs]()
-        { return lhs.width() - rhs.width(); },
-        [lhs, rhs]()
-        { return lhs.height() - rhs.height(); }};
+        { return std::make_tuple(lhs.width() - rhs.width(), lhs.height() - rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator*(typename usagi::size<Type>::value_type &&lhs, const usagi::size<Type> &rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator*(typename SizeType::value_type &&lhs, const SizeType &rhs)
   {
     assert(lhs >= 0);
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs * rhs.width(); },
-        [lhs, rhs]()
-        { return lhs * rhs.height(); }};
+        { return std::make_tuple(lhs * rhs.width(), lhs * rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator*(const usagi::size<Type> &lhs, typename usagi::size<Type>::value_type &&rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator*(const SizeType &lhs, typename SizeType::value_type &&rhs)
   {
     assert(rhs >= 0);
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs.width() * rhs; },
-        [lhs, rhs]()
-        { return lhs.height() * rhs; }};
+        { return std::make_tuple(lhs.width() * rhs, lhs.height() * rhs); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator*(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr decltype(auto) operator*(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeTypeA::value_type>{
         [lhs, rhs]()
-        { return lhs.width() * rhs.width(); },
-        [lhs, rhs]()
-        { return lhs.height() * rhs.height(); }};
+        { return std::make_tuple(lhs.width() * rhs.width(), lhs.height() * rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator/(typename usagi::size<Type>::value_type &&lhs, const usagi::size<Type> &rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator/(typename SizeType::value_type &&lhs, const SizeType &rhs)
   {
     assert(lhs >= 0);
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs / rhs.width(); },
-        [lhs, rhs]()
-        { return lhs / rhs.height(); }};
+        { return std::make_tuple(lhs / rhs.width(), lhs / rhs.height()); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator/(const usagi::size<Type> &lhs, typename usagi::size<Type>::value_type &&rhs)
+  template <class SizeType>
+  inline constexpr decltype(auto) operator/(const SizeType &lhs, typename SizeType::value_type &&rhs)
   {
     assert(rhs > 0);
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeType::value_type>{
         [lhs, rhs]()
-        { return lhs.width() / rhs; },
-        [lhs, rhs]()
-        { return lhs.height() / rhs; }};
+        { return std::make_tuple(lhs.width() / rhs, lhs.height() / rhs); }};
   }
 
-  template <class Type>
-  inline constexpr decltype(auto) operator/(const usagi::size<Type> &lhs, const usagi::size<Type> &rhs)
+  template <
+      class SizeTypeA,
+      class SizeTypeB,
+      class = std::enable_if_t<std::is_same_v<typename SizeTypeA::value_type, typename SizeTypeB::value_type>>>
+  inline constexpr decltype(auto) operator/(const SizeTypeA &lhs, const SizeTypeB &rhs)
   {
-    return usagi::size<Type>{
+    return usagi::paired_size<typename SizeTypeA::value_type>{
         [lhs, rhs]()
-        { return lhs.width() / rhs.width(); },
-        [lhs, rhs]()
-        { return lhs.height() / rhs.height(); }};
+        { return std::make_tuple(lhs.width() / rhs.width(), lhs.height() / rhs.height()); }};
   }
 }
--- include/usagi/geometry/size/size.h
+++ include/usagi/geometry/size/size.h
@@ -3,6 +3,7 @@
 #include <cassert>
 
 #include <usagi/variable/variable_traits.h>
+#include <usagi/utility/mono_tuple.h>
 
 namespace usagi
 {
@@ -32,9 +33,55 @@ namespace usagi
       return h;
     }
 
+    std::pair<value_type, value_type> operator()() const
+    {
+      return {width(), height()};
+    }
+
     size<value_type> duplicate() const { return size<value_type>{width(), height()}; }
 
   private:
     variable_type width_, height_;
   };
+
+  // pair特殊化
+  template <class Type, class = std::enable_if_t<std::is_arithmetic_v<Type>>>
+  struct paired_size
+  {
+    using value_type = Type;
+    using pair_type = usagi::utility::mono_tuple<value_type, 2>;
+    using size_type = typename usagi::variable_traits<pair_type>::value_type;
+    using variable_type = typename usagi::variable_traits<pair_type>::variable_type;
+
+    constexpr paired_size() : functor{} {}
+    constexpr paired_size(variable_type s) : functor{s} {}
+
+    value_type width() const
+    {
+      [[maybe_unused]] auto [w, _] = functor();
+      assert(w > 0);
+      return w;
+    }
+
+    value_type height() const
+    {
+      [[maybe_unused]] auto [_, h] = functor();
+      assert(h > 0);
+      return h;
+    }
+
+    size_type operator()() const
+    {
+      auto [w, h] = functor();
+      assert(w > 0);
+      assert(h > 0);
+      return {w, h};
+    }
+
+    size<value_type> duplicate() const { return size<value_type>{functor()}; }
+
+  private:
+    variable_type functor;
+  };
+
 }

25461ad8817915c5e034acd850a187f494e2f5fb
SachiSakurane <sakuranesachi@gmail.com>
2021-06-28 23:45:39 +0900

    add unclickable test

--- test/concepts/clickable.cpp
+++ test/concepts/clickable.cpp
@@ -12,9 +12,20 @@ struct Clickable
   void event(typename mouse_traits::on_over_type);
 };
 
+template <class ValueType>
+struct UnClickable
+{
+  using value_type = ValueType;
+  using mouse_traits = typename usagi::type::mouse::mouse_traits<value_type>;
+  void event(typename mouse_traits::on_down_type);
+  void event(typename mouse_traits::on_drag_type);
+  void event(typename mouse_traits::on_up_type);
+};
+
 bool ClickableStaticTest()
 {
   static_assert(usagi::concepts::clickable<Clickable<int>>);
+  static_assert(!usagi::concepts::clickable<UnClickable<int>>);
 
   return true;
 }

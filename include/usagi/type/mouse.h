f88c06bde9724b3e566b042ad2ea14ff8b991805
SachiSakurane <sakuranesachi@gmail.com>
2021-06-28 23:31:21 +0900

    fix mouse

--- include/usagi/type/mouse.h
+++ include/usagi/type/mouse.h
@@ -9,26 +9,50 @@ namespace usagi::type
     template <class ValueType, class = std::enable_if_t<std::is_arithmetic_v<ValueType>>>
     struct on_down
     {
+      on_down() = default;
+      on_down(ValueType _x, ValueType _y) : x{_x}, y{_y} {}
+
       ValueType x, y;
     };
 
     template <class ValueType, class = std::enable_if_t<std::is_arithmetic_v<ValueType>>>
     struct on_drag
     {
+      on_drag() = default;
+      on_drag(ValueType _x, ValueType _y) : x{_x}, y{_y} {}
+
       ValueType x, y;
     };
 
     template <class ValueType, class = std::enable_if_t<std::is_arithmetic_v<ValueType>>>
     struct on_up
     {
+      on_up() = default;
+      on_up(ValueType _x, ValueType _y) : x{_x}, y{_y} {}
+
       ValueType x, y;
     };
 
     template <class ValueType, class = std::enable_if_t<std::is_arithmetic_v<ValueType>>>
     struct on_over
     {
+      on_over() = default;
+      on_over(ValueType _x, ValueType _y) : x{_x}, y{_y} {}
+
       ValueType x, y;
     };
+
+    template <class ValueType>
+    on_down(ValueType, ValueType) -> on_down<ValueType>;
+
+    template <class ValueType>
+    on_drag(ValueType, ValueType) -> on_drag<ValueType>;
+
+    template <class ValueType>
+    on_up(ValueType, ValueType) -> on_up<ValueType>;
+
+    template <class ValueType>
+    on_over(ValueType, ValueType) -> on_over<ValueType>;
 
     template <class ValueType>
     struct mouse_traits

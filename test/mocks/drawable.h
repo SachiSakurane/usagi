c50c5e3c3ac7f37135fbf87476656668fb514919
SachiSakurane <sakuranesachi@gmail.com>
2021-06-29 22:35:47 +0900

    draw contextable

--- test/mocks/drawable.h
+++ test/mocks/drawable.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <usagi/concepts/graphics/draw_contextable.h>
 #include <usagi/geometry/rect/rect.h>
 
-template <class ValueType, class DrawContextType>
+template <class ValueType, usagi::concepts::graphics::draw_contextable DrawContextType>
 struct Drawable
 {
   using value_type = ValueType;
@@ -11,7 +12,7 @@ struct Drawable
   void draw(rect_type, draw_context_type &);
 };
 
-template <class ValueType, class DrawContextType>
+template <class ValueType, usagi::concepts::graphics::draw_contextable DrawContextType>
 struct UnDrawable
 {
   using value_type = ValueType;

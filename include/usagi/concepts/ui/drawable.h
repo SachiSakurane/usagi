754afb1249097d5780b85380d5df5c5a009ba04f
SachiSakurane <sakuranesachi@gmail.com>
2021-06-29 19:47:48 +0900

    drawable

--- include/usagi/concepts/ui/drawable.h
+++ include/usagi/concepts/ui/drawable.h
@@ -3,6 +3,7 @@
 #include <type_traits>
 #include <utility>
 
+#include <usagi/concepts/geometry/rect_concept.h>
 #include <usagi/concepts/graphics/draw_contextable.h>
 
 namespace usagi::concepts::ui
@@ -12,10 +13,16 @@ namespace usagi::concepts::ui
    * draw_context_type は draw_context を満たす 
   */
   template <class DrawableType>
-  concept drawable = usagi::concepts::graphics::draw_contextable<typename DrawableType::draw_context_type> && requires(DrawableType &d)
+  concept drawable =
+      usagi::concepts::geometry::rect_concept<typename DrawableType::rect_type> &&
+      usagi::concepts::graphics::draw_contextable<typename DrawableType::draw_context_type> &&
+      requires(DrawableType &d)
   {
     typename DrawableType::draw_context_type;
+    typename DrawableType::rect_type;
 
-    d.draw(std::declval<std::add_lvalue_reference_t<typename DrawableType::draw_context_type>>());
+    d.draw(
+        std::declval<typename DrawableType::rect_type>(),
+        std::declval<std::add_lvalue_reference_t<typename DrawableType::draw_context_type>>());
   };
 }
--- test/concepts/ui/drawable.cpp
+++ test/concepts/ui/drawable.cpp
@@ -1,19 +1,22 @@
 #include <gtest/gtest.h>
 #include <usagi/concepts/graphics/color_concept.h>
 #include <usagi/concepts/ui/drawable.h>
+#include <usagi/geometry/rect/rect.h>
 
 template <class DrawContextType>
 struct Drawable
 {
   using draw_context_type = DrawContextType;
-  void draw(draw_context_type &draw);
+  using rect_type = usagi::geometry::rect<float>;
+  void draw(rect_type, draw_context_type &);
 };
 
 template <class DrawContextType>
 struct UnDrawable
 {
   using draw_context_type = DrawContextType;
-  // void draw(draw_context_type &draw);
+  using rect_type = usagi::geometry::rect<float>;
+  // void draw(rect_type, draw_context_type &draw);
 };
 
 struct DrawContext

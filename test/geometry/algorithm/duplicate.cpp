b03c1adc35da023d72fa9087029e12100cee2b2b
SachiSakurane <sakuranesachi@gmail.com>
2021-06-30 18:17:43 +0900

    fix eq

--- test/geometry/algorithm/duplicate.cpp
+++ test/geometry/algorithm/duplicate.cpp
@@ -8,7 +8,7 @@ TEST(GeometryAlgorithmTest, DuplicateCase)
   usagi::geometry::point<float> s{42.f, [&side]()
                                   { return side; }};
   auto c = usagi::geometry::duplicate(s);
-  ASSERT_TRUE(s == c);
+  ASSERT_EQ(s, c);
   side = 10.f;
-  ASSERT_TRUE(s != c);
+  ASSERT_NE(s, c);
 }

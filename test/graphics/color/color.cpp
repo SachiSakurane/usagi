4d017d18e1588a90dd69e3d11084999f266315df
SachiSakurane <sakuranesachi@gmail.com>
2021-06-30 18:22:21 +0900

    fix eq

--- test/graphics/color/color.cpp
+++ test/graphics/color/color.cpp
@@ -23,20 +23,20 @@ TEST(ColorTest, CommonCase)
       0.42f,
       []()
       { return 0.042f; }};
-  ASSERT_TRUE(c.r() == 42.f);
-  ASSERT_TRUE(c.g() == 4.2f);
-  ASSERT_TRUE(c.b() == 0.42f);
-  ASSERT_TRUE(c.a() == 0.042f);
+  ASSERT_EQ(c.r(), 42.f);
+  ASSERT_EQ(c.g(), 4.2f);
+  ASSERT_EQ(c.b(), 0.42f);
+  ASSERT_EQ(c.a(), 0.042f);
   auto cc = c;
   c = {24.f,
        2.4f,
        0.24f,
        []()
        { return 0.024f; }};
-  ASSERT_TRUE(c.r() == 24.f);
-  ASSERT_TRUE(c.a() == 0.024f);
-  ASSERT_TRUE(cc.r() == 42.f);
-  ASSERT_TRUE(cc.a() == 0.042f);
+  ASSERT_EQ(c.r(), 24.f);
+  ASSERT_EQ(c.a(), 0.024f);
+  ASSERT_EQ(cc.r(), 42.f);
+  ASSERT_EQ(cc.a(), 0.042f);
 }
 
 TEST(ColorTest, DuplicateCase)
@@ -48,9 +48,9 @@ TEST(ColorTest, DuplicateCase)
       0.42f, [&side]()
       { return side; }};
   auto copied = c.duplicate();
-  ASSERT_TRUE(copied.r() == 42.f);
-  ASSERT_TRUE(copied.a() == 0.042f);
+  ASSERT_EQ(copied.r(), 42.f);
+  ASSERT_EQ(copied.a(), 0.042f);
   side = 20.f;
-  ASSERT_TRUE(c.a() == 20.f);
-  ASSERT_TRUE(copied.a() == 0.042f);
+  ASSERT_EQ(c.a(), 20.f);
+  ASSERT_EQ(copied.a(), 0.042f);
 }

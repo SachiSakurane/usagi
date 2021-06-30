94934580f83b31224dcfafe939d337ae9088718e
SachiSakurane <sakuranesachi@gmail.com>
2021-06-30 18:28:01 +0900

    fix test

--- test/variable/variable.cpp
+++ test/variable/variable.cpp
@@ -3,26 +3,27 @@
 
 TEST(VariableTest, ConstantCase)
 {
-  ASSERT_TRUE(usagi::variable{42}() == 42);
+  ASSERT_EQ(usagi::variable{42}(), 42);
   auto va = usagi::variable{42};
-  ASSERT_TRUE(va() == 42);
+  ASSERT_EQ(va(), 42);
   auto v = 42;
-  ASSERT_TRUE(usagi::variable{v}() == 42);
+  ASSERT_EQ(usagi::variable{v}(), 42);
 
-  ASSERT_TRUE(usagi::variable{std::string{"42"}}() == "42");
+  ASSERT_EQ(usagi::variable{std::string{"42"}}(), "42");
 }
 
 TEST(VariableTest, FunctionCase)
 {
-  ASSERT_TRUE(usagi::variable{[]()
-                              { return 42; }}() == 42);
+  ASSERT_EQ(usagi::variable{[]()
+                            { return 42; }}(), 42);
   auto va = usagi::variable{[]()
                             { return 42; }};
-  ASSERT_TRUE(va() == 42);
+  ASSERT_EQ(va(), 42);
   auto v = []()
   { return 42; };
-  ASSERT_TRUE(usagi::variable{v}() == 42);
+  ASSERT_EQ(usagi::variable{v}(), 42);
 
-  ASSERT_TRUE(usagi::variable{[]()
-                              { return std::string{"42"}; }}() == "42");
+  ASSERT_EQ(usagi::variable{[]()
+                            { return std::string{"42"}; }}(),
+            "42");
 }

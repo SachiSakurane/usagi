6e8d6b95172a69b95b4474e1d20b83dbb0bd0ea9
SachiSakurane <sakuranesachi@gmail.com>
2021-07-07 09:24:53 +0900

    add padding

--- include/usagi/geometry/rect/function.hpp
+++ include/usagi/geometry/rect/function.hpp
@@ -1,11 +1,34 @@
 #pragma once
 
+#include <algorithm>
+
 #include <usagi/concepts/geometry.hpp>
+#include <usagi/geometry/rect/rect.hpp>
 
 namespace usagi::geometry
 {
   inline constexpr bool contain(const usagi::concepts::geometry::rect_concept auto &r, const usagi::concepts::geometry::point_concept auto &p)
   {
     return (r.l() <= p.x() && p.x() <= r.r()) && (r.t() <= p.y() && p.y() <= r.b());
+  }
+
+  template <usagi::concepts::geometry::rect_concept RectType>
+  inline constexpr decltype(auto) padding(const RectType &rect, typename RectType::value_type v)
+  {
+    return usagi::geometry::paired_rect<typename RectType::value_type>{
+        [rect, v]()
+        {
+          auto l = rect.l() + v;
+          auto t = rect.t() + v;
+          auto r = rect.r() - v;
+          auto b = rect.b() - v;
+          bool w = l < r;
+          bool h = t < b;
+          return std::make_tuple(
+              w ? l : (l + r) / static_cast<typename RectType::value_type>(2),
+              h ? t : (t + b) / static_cast<typename RectType::value_type>(2),
+              w ? r : (l + r) / static_cast<typename RectType::value_type>(2),
+              h ? b : (t + b) / static_cast<typename RectType::value_type>(2));
+        }};
   }
 }
--- include/usagi/geometry/rect/rect.hpp
+++ include/usagi/geometry/rect/rect.hpp
@@ -13,14 +13,14 @@ namespace usagi::geometry
   {
   public:
     using value_type = typename usagi::variable_traits<Type>::value_type;
-    using variable_type = typename usagi::variable_traits<Type>::variable_type;
+    using variable_type = typename usagi::variable_traits<value_type>::variable_type;
     using size_type = usagi::geometry::size<value_type>;
     using point_type = usagi::geometry::point<value_type>;
 
     constexpr rect() : left{}, top{}, right{}, bottom{} {}
     constexpr rect(variable_type l, variable_type t, variable_type r, variable_type b) : left{l}, top{t}, right{r}, bottom{b} {}
-    constexpr explicit rect(const size_type &size) : rect(point_type{}, size) {}
-    constexpr rect(const point_type &point, const size_type &size)
+    constexpr explicit rect(const usagi::concepts::geometry::size_concept auto &size) : rect(point_type{}, size) {}
+    constexpr rect(const usagi::concepts::geometry::point_concept auto &point, const usagi::concepts::geometry::size_concept auto &size)
         : left{[point]()
                { return point.x(); }},
           top{[point]()
@@ -72,9 +72,70 @@ namespace usagi::geometry
     variable_type left, top, right, bottom;
   };
 
-  template <usagi::utility::arithmetic Type>
-  rect(const usagi::geometry::size<Type> &) -> rect<Type>;
+  template <usagi::concepts::geometry::size_concept SizeType>
+  rect(const SizeType &) -> rect<typename SizeType::value_type>;
 
+  template <
+      usagi::concepts::geometry::point_concept PointType,
+      usagi::concepts::geometry::size_concept SizeType,
+      class = std::enable_if_t<std::is_same_v<typename PointType::value_type, typename SizeType::value_type>>>
+  rect(const PointType &, const SizeType &) -> rect<typename SizeType::value_type>;
+
+  /**
+   * 
+  */
   template <usagi::utility::arithmetic Type>
-  rect(const usagi::geometry::point<Type> &, const usagi::geometry::size<Type> &) -> rect<Type>;
+  struct paired_rect
+  {
+  public:
+    using value_type = typename usagi::variable_traits<Type>::value_type;
+    using pair_type = usagi::utility::mono_tuple<value_type, 2>;
+    using variable_type = typename usagi::variable_traits<pair_type>::variable_type;
+    using size_type = usagi::geometry::size<value_type>;
+    using point_type = usagi::geometry::point<value_type>;
+
+    constexpr paired_rect() : functor{} {}
+    constexpr paired_rect(variable_type v) : functor{v} {}
+
+    value_type l() const
+    {
+      auto v = std::get<0>(functor());
+      assert(v <= std::get<2>(functor()));
+      return v;
+    }
+
+    value_type t() const
+    {
+      auto v = std::get<1>(functor());
+      assert(v <= std::get<3>(functor()));
+      return v;
+    }
+
+    value_type r() const
+    {
+      auto v = std::get<2>(functor());
+      assert(std::get<0>(functor()) <= v);
+      return v;
+    }
+
+    value_type b() const
+    {
+      auto v = std::get<3>(functor());
+      assert(std::get<1>(functor()) <= v);
+      return v;
+    }
+
+    size_type size() const
+    {
+      return size_type{[f = this->functor]()
+                       { return std::get<2>(f()) - std::get<0>(f()); },
+                       [f = this->functor]()
+                       { return std::get<3>(f()) - std::get<1>(f()); }};
+    }
+    point_type center() const { return point_type{size() / static_cast<value_type>(2)}; }
+    paired_rect<value_type> duplicate() const { return paired_rect<value_type>{functor()}; }
+
+  private:
+    variable_type functor;
+  };
 }

a30f1841d93c49e01498b0ce5afaf2e3016beb51
SachiSakurane <sakuranesachi@gmail.com>
2021-07-10 09:48:06 +0900

    covered

--- test/ui/view.cpp
+++ test/ui/view.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <usagi/concepts/ui/viewable.hpp>
+#include <usagi/geometry/rect/operator.hpp>
 #include <usagi/ui/view.hpp>
 
 struct DrawContextable {
@@ -19,6 +20,13 @@ static_assert(usagi::concepts::ui::viewable<decltype(usagi::ui::view{(SpecialVie
               "view is constructable from SpecialView");
 } // namespace
 
+TEST(ViewTest, BoundsCase) {
+  auto v = usagi::ui::view{usagi::ui::base_view<float, DrawContextable>{
+      usagi::geometry::rect<float>{10.f, 10.f, 40.f, 40.f}}};
+  ASSERT_EQ(v.bounds(), (usagi::geometry::size<float>{30.f, 30.f}));
+  ASSERT_EQ(v.frame(), (usagi::geometry::rect<float>{10.f, 10.f, 40.f, 40.f}));
+}
+
 TEST(ViewTest, PredicationCase) {
   {
     auto v = usagi::ui::view<float, DrawContextable>{};
@@ -34,10 +42,29 @@ TEST(ViewTest, DrawCase) {
   auto v = usagi::ui::view{SpecialView{}};
   auto context = DrawContextable{};
   v.draw(context);
+
+  // sub case
+  v.add_sub_view(usagi::ui::base_view<float, DrawContextable>{});
+  v.draw(context);
 }
 
 TEST(ViewTest, SubViewCase) {
   auto v = usagi::ui::view{SpecialView{}};
   auto &sub = v.add_sub_view(usagi::ui::base_view<float, DrawContextable>{});
   ASSERT_TRUE(static_cast<bool>(sub));
+}
+
+TEST(ViewTest, ClickCase) {
+  auto v = usagi::ui::view{SpecialView{}};
+  v.event(usagi::type::mouse_traits<float>::on_down_type{});
+  v.event(usagi::type::mouse_traits<float>::on_drag_type{});
+  v.event(usagi::type::mouse_traits<float>::on_over_type{});
+  v.event(usagi::type::mouse_traits<float>::on_up_type{});
+
+  // sub case
+  v.add_sub_view(usagi::ui::base_view<float, DrawContextable>{});
+  v.event(usagi::type::mouse_traits<float>::on_down_type{});
+  v.event(usagi::type::mouse_traits<float>::on_drag_type{});
+  v.event(usagi::type::mouse_traits<float>::on_over_type{});
+  v.event(usagi::type::mouse_traits<float>::on_up_type{});
 }

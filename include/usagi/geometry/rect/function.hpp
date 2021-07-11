6176764a3a9221cb859591adeaa3e8e3bc15b395
SachiSakurane <sakuranesachi@gmail.com>
2021-07-12 00:27:04 +0900

    add from

--- include/usagi/geometry/rect/function.hpp
+++ include/usagi/geometry/rect/function.hpp
@@ -6,28 +6,48 @@
 #include <usagi/geometry/rect/rect.hpp>
 
 namespace usagi::geometry {
-inline constexpr bool contain(const concepts::geometry::rect_concept auto &r,
-                              const concepts::geometry::point_concept auto &p) {
-  return (r.l() <= p.x() && p.x() <= r.r()) &&
-         (r.t() <= p.y() && p.y() <= r.b());
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) from_left(const RectType &rect, typename RectType::value_type l) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, l]() { return std::make_tuple(l, rect.t(), rect.r(), rect.b()); }};
 }
 
 template <concepts::geometry::rect_concept RectType>
-inline constexpr decltype(auto) padding(const RectType &rect,
-                                        typename RectType::value_type v) {
+inline constexpr decltype(auto) from_top(const RectType &rect, typename RectType::value_type t) {
   return tupled_rect<typename RectType::value_type>{
-      [rect, v]() {
-        auto l = rect.l() + v;
-        auto t = rect.t() + v;
-        auto r = rect.r() - v;
-        auto b = rect.b() - v;
-        bool w = l < r;
-        bool h = t < b;
-        return std::make_tuple(
-            w ? l : (l + r) / static_cast<typename RectType::value_type>(2),
-            h ? t : (t + b) / static_cast<typename RectType::value_type>(2),
-            w ? r : (l + r) / static_cast<typename RectType::value_type>(2),
-            h ? b : (t + b) / static_cast<typename RectType::value_type>(2));
-      }};
+      [rect, t]() { return std::make_tuple(rect.l(), t, rect.r(), rect.b()); }};
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) from_right(const RectType &rect, typename RectType::value_type r) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, r]() { return std::make_tuple(rect.l(), rect.t(), r, rect.b()); }};
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) from_bottom(const RectType &rect, typename RectType::value_type b) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, b]() { return std::make_tuple(rect.l(), rect.t(), rect.r(), b); }};
+}
+
+inline constexpr bool contain(const concepts::geometry::rect_concept auto &r,
+                              const concepts::geometry::point_concept auto &p) {
+  return (r.l() <= p.x() && p.x() <= r.r()) && (r.t() <= p.y() && p.y() <= r.b());
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) padding(const RectType &rect, typename RectType::value_type v) {
+  return tupled_rect<typename RectType::value_type>{[rect, v]() {
+    auto l = rect.l() + v;
+    auto t = rect.t() + v;
+    auto r = rect.r() - v;
+    auto b = rect.b() - v;
+    bool w = l < r;
+    bool h = t < b;
+    return std::make_tuple(w ? l : (l + r) / static_cast<typename RectType::value_type>(2),
+                           h ? t : (t + b) / static_cast<typename RectType::value_type>(2),
+                           w ? r : (l + r) / static_cast<typename RectType::value_type>(2),
+                           h ? b : (t + b) / static_cast<typename RectType::value_type>(2));
+  }};
 }
 } // namespace usagi::geometry
--- test/geometry/rect/function.cpp
+++ test/geometry/rect/function.cpp
@@ -2,6 +2,26 @@
 #include <usagi/geometry/point/point.hpp>
 #include <usagi/geometry/rect.hpp>
 
+TEST(RectFunctionTest, FromCase) {
+  usagi::geometry::rect<float> r{2.f, 4.f, 42.f, 24.f};
+  {
+    auto rr = usagi::geometry::from_left(r, 2.4f);
+    ASSERT_EQ(rr.l(), 2.4f);
+  }
+  {
+    auto rr = usagi::geometry::from_top(r, 2.4f);
+    ASSERT_EQ(rr.t(), 2.4f);
+  }
+  {
+    auto rr = usagi::geometry::from_right(r, 42.24f);
+    ASSERT_EQ(rr.r(), 42.24f);
+  }
+  {
+    auto rr = usagi::geometry::from_bottom(r, 42.24f);
+    ASSERT_EQ(rr.b(), 42.24f);
+  }
+}
+
 TEST(RectFunctionTest, ContainCase) {
   usagi::geometry::rect<float> a{0.f, 0.f, 42.f, 42.f};
   ASSERT_TRUE(usagi::geometry::contain(a, usagi::geometry::point<float>{3.f, 4.f}));

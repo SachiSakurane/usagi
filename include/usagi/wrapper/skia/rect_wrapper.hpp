62d71506c443ae73c6d5e9dcbf602d49318daf0b
SachiSakurane <sakuranesachi@gmail.com>
2021-07-12 00:08:00 +0900

    add skia wrapper

---
+++ include/usagi/wrapper/skia/rect_wrapper.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <usagi/concepts/geometry.hpp>
+
+namespace usagi::wrapper::skia {
+
+template <usagi::concepts::geometry::size_concept SizeType>
+SkRect to_rect(SizeType &&size) {
+  return SkRect::MakeWH(size.width(), size.height());
+}
+
+template <usagi::concepts::geometry::point_concept PointType,
+          usagi::concepts::geometry::size_concept SizeType>
+SkRect to_rect(PointType &&point, SizeType &&size) {
+  return SkRect::MakeXYWH(point.x(), point.y(), size.width(), size.height());
+}
+
+template <usagi::concepts::geometry::rect_concept RectType>
+SkRect to_rect(RectType &&rect) {
+  auto [l, t, r, b] = std::forward<RectType>(rect)();
+  return SkRect::MakeLTRB(l, t, r, b);
+}
+
+} // namespace usagi::wrapper::skia

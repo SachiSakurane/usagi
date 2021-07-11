8106d7778ee2e54fda4e3f00af65f597fcad5a2f
SachiSakurane <sakuranesachi@gmail.com>
2021-07-12 00:39:21 +0900

    add reduce_from

--- include/usagi/geometry/rect/function.hpp
+++ include/usagi/geometry/rect/function.hpp
@@ -30,6 +30,34 @@ inline constexpr decltype(auto) from_bottom(const RectType &rect, typename RectT
       [rect, b]() { return std::make_tuple(rect.l(), rect.t(), rect.r(), b); }};
 }
 
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) reduce_from_left(const RectType &rect,
+                                                 typename RectType::value_type l) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, l]() { return std::make_tuple(rect.l() + l, rect.t(), rect.r(), rect.b()); }};
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) reduce_from_top(const RectType &rect,
+                                                typename RectType::value_type t) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, t]() { return std::make_tuple(rect.l(), rect.t() + t, rect.r(), rect.b()); }};
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) reduce_from_right(const RectType &rect,
+                                                  typename RectType::value_type r) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, r]() { return std::make_tuple(rect.l(), rect.t(), rect.r() - r, rect.b()); }};
+}
+
+template <concepts::geometry::rect_concept RectType>
+inline constexpr decltype(auto) reduce_from_bottom(const RectType &rect,
+                                                   typename RectType::value_type b) {
+  return tupled_rect<typename RectType::value_type>{
+      [rect, b]() { return std::make_tuple(rect.l(), rect.t(), rect.r(), rect.b() - b); }};
+}
+
 inline constexpr bool contain(const concepts::geometry::rect_concept auto &r,
                               const concepts::geometry::point_concept auto &p) {
   return (r.l() <= p.x() && p.x() <= r.r()) && (r.t() <= p.y() && p.y() <= r.b());
--- test/geometry/rect/function.cpp
+++ test/geometry/rect/function.cpp
@@ -22,6 +22,30 @@ TEST(RectFunctionTest, FromCase) {
   }
 }
 
+TEST(RectFunctionTest, ReduceFromCase) {
+  usagi::geometry::rect<float> rect{0.f, 0.f, 42.f, 42.f};
+  {
+    auto rr = usagi::geometry::reduce_from_left(rect, 1.f);
+    ASSERT_EQ(rr.l(), 1.f);
+    ASSERT_EQ(rr.size().width(), 41.f);
+  }
+  {
+    auto rr = usagi::geometry::reduce_from_top(rect, 1.f);
+    ASSERT_EQ(rr.t(), 1.f);
+    ASSERT_EQ(rr.size().height(), 41.f);
+  }
+  {
+    auto rr = usagi::geometry::reduce_from_right(rect, 2.f);
+    ASSERT_EQ(rr.r(), 40.f);
+    ASSERT_EQ(rr.size().width(), 40.f);
+  }
+  {
+    auto rr = usagi::geometry::reduce_from_bottom(rect, 2.f);
+    ASSERT_EQ(rr.b(), 40.f);
+    ASSERT_EQ(rr.size().height(), 40.f);
+  }
+}
+
 TEST(RectFunctionTest, ContainCase) {
   usagi::geometry::rect<float> a{0.f, 0.f, 42.f, 42.f};
   ASSERT_TRUE(usagi::geometry::contain(a, usagi::geometry::point<float>{3.f, 4.f}));

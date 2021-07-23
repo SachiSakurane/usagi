141106bcb8c701d778318a8d1b65bb910792884a
SachiSakurane <sakuranesachi@gmail.com>
2021-07-23 23:37:00 +0900

    adapter

--- include/usagi/ui/surface.hpp
+++ include/usagi/ui/surface.hpp
@@ -7,14 +7,17 @@
 #include <usagi/geometry/geometry_traits.hpp>
 
 namespace usagi::ui {
-
-namespace detail {
+template <class FunctionType>
+struct surface_wrapper {
   template <usagi::concepts::ui::viewable ViewType>
-  using surface_func_type =
-      std::function<void(typename ViewType::draw_context_type &, const ViewType &)>;
-} // namespace detail
+  constexpr void operator()(typename ViewType::draw_context_type &c, const ViewType &v) {
+    func(c, v);
+  }
 
-template <usagi::concepts::ui::viewable ViewType>
+  FunctionType func;
+};
+
+template <usagi::concepts::ui::viewable ViewType, class FunctionType>
 struct surface final {
   using value_type = typename ViewType::value_type;
   using rect_type = typename usagi::geometry::geometry_traits<value_type>::rect_type;
@@ -23,10 +26,8 @@ struct surface final {
   using mouse_traits = typename usagi::type::mouse_traits<value_type>;
   using view_type = typename ViewType::view_type;
 
-  template <class... Args>
-  explicit surface(usagi::ui::detail::surface_func_type<ViewType> &&f, Args &&...args)
-      : drawer{std::forward<usagi::ui::detail::surface_func_type<ViewType>>(f)},
-        holder{std::forward<Args>(args)...} {}
+  surface(ViewType &&v, FunctionType f)
+      : holder{std::move(v)}, drawer{f} {}
 
   void draw(draw_context_type &context) {
     drawer(context, holder);
@@ -46,15 +47,21 @@ struct surface final {
   }
 
 private:
-  usagi::ui::detail::surface_func_type<ViewType> drawer;
   ViewType holder;
+  FunctionType drawer;
 };
 
-template <usagi::concepts::ui::viewable ViewType, class... ConstructorArgs>
-inline constexpr decltype(auto) surfaced(usagi::ui::detail::surface_func_type<ViewType> &&draw_func,
-                                         ConstructorArgs &&...args) {
-  return usagi::ui::surface<ViewType>{
-      std::forward<usagi::ui::detail::surface_func_type<ViewType>>(draw_func),
-      std::forward<ConstructorArgs>(args)...};
+template <usagi::concepts::ui::viewable ViewType, class FunctionType>
+surface(ViewType &&, FunctionType) -> surface<ViewType, FunctionType>;
+
+template <usagi::concepts::ui::viewable ViewType, class FunctionType>
+inline constexpr decltype(auto) operator|(ViewType &&v, surface_wrapper<FunctionType> func) {
+  return surface{std::forward<ViewType>(v), func};
 }
+
+template <class FunctionType>
+inline constexpr decltype(auto) surfaced(FunctionType func) {
+  return surface_wrapper<FunctionType>{func};
+}
+
 } // namespace usagi::ui
--- test/ui/surface.cpp
+++ test/ui/surface.cpp
@@ -32,19 +32,21 @@ private:
 
 // static test
 namespace {
-static_assert(usagi::concepts::ui::viewable<usagi::ui::surface<SpecificView<int, DrawContext>>>,
+using local_view_type = SpecificView<int, DrawContext>;
+static_assert(usagi::concepts::ui::viewable<usagi::ui::surface<
+                  local_view_type,
+                  usagi::ui::surface_wrapper<std::function<void(
+                      typename local_view_type::draw_context_type &, const local_view_type &)>>>>,
               "usagi::ui::surface<SpecificView<int, DrawContext>> has viewable concept");
-}
+} // namespace
 
 TEST(SurfaceTest, SequentialCase) {
   std::vector<int> stamp;
-  auto s =
-      usagi::ui::surface<SpecificView<int, DrawContext>>{[&stamp](auto &context, const auto &) {
-                                                           // 0
-                                                           stamp.emplace_back(0);
-                                                           context.tick();
-                                                         },
-                                                         stamp};
+  auto s = usagi::ui::surface{local_view_type{stamp}, [&stamp](auto &context, const auto &) {
+                                // 0
+                                stamp.emplace_back(0);
+                                context.tick();
+                              }};
   auto context = DrawContext{stamp};
   s.draw(context);
 
@@ -58,13 +60,11 @@ TEST(SurfaceTest, SequentialCase) {
 
 TEST(SurfaceTest, SurfacedCase) {
   std::vector<int> stamp;
-  auto s = usagi::ui::surfaced<SpecificView<int, DrawContext>>(
-      [&stamp](auto &context, const auto &) {
-        // 0
-        stamp.emplace_back(0);
-        context.tick();
-      },
-      stamp);
+  auto s = local_view_type{stamp} | usagi::ui::surfaced([&stamp](auto &context, const auto &) {
+             // 0
+             stamp.emplace_back(0);
+             context.tick();
+           });
   auto context = DrawContext{stamp};
   s.draw(context);
 

5b953658607aef570ba4cbb70d9fdd114fa4a91c
SachiSakurane <sakuranesachi@gmail.com>
2021-07-25 02:17:25 +0900

    add gesture

---
+++ include/usagi/ui/gesture.hpp
@@ -0,0 +1,155 @@
+#pragma once
+
+#include <tuple>
+#include <utility>
+
+#include <usagi/concepts/ui/viewable.hpp>
+#include <usagi/geometry/geometry_traits.hpp>
+#include <usagi/utility/invocable.hpp>
+
+namespace usagi::ui {
+namespace detail {
+  template <class Func, class ArgsTuple, std::size_t... Sequence>
+  inline constexpr bool is_apply_invocable(std::index_sequence<Sequence...>) {
+    return std::is_invocable_v<Func, std::tuple_element_t<Sequence, ArgsTuple>...>;
+  }
+
+  template <class Func, class SearchArgsTuple>
+  concept apply_invocable = is_apply_invocable<Func, SearchArgsTuple>(
+      std::make_index_sequence<std::tuple_size_v<SearchArgsTuple>>());
+
+  template <class SearchArgsTuple, apply_invocable<SearchArgsTuple> Default, class... Args>
+  struct pick_func;
+
+  // iterate
+  template <class SearchArgsTuple, apply_invocable<SearchArgsTuple> Default, class Front,
+            class... Args>
+  struct pick_func<SearchArgsTuple, Default, Front, Args...>
+      : pick_func<SearchArgsTuple, Default, Args...> {
+    pick_func<SearchArgsTuple, Default, Front, Args...>(Default &&d, Front &&, Args &&...args)
+        : pick_func<SearchArgsTuple, Default, Args...>{std::forward<Default>(d),
+                                                       std::forward<Args>(args)...} {}
+  };
+
+  // found apply_invocable
+  template <class SearchArgsTuple, apply_invocable<SearchArgsTuple> Default,
+            apply_invocable<SearchArgsTuple> Front, class... Args>
+  struct pick_func<SearchArgsTuple, Default, Front, Args...> {
+    pick_func<SearchArgsTuple, Default, Front, Args...>(Default &&, Front &&f, Args &&...)
+        : elem{std::forward<Front>(f)} {}
+    Front elem;
+  };
+
+  // no result(return default)
+  template <class SearchArgsTuple, apply_invocable<SearchArgsTuple> Default>
+  struct pick_func<SearchArgsTuple, Default> {
+    explicit pick_func<SearchArgsTuple, Default>(Default &&d) : elem{std::forward<Default>(d)} {}
+    Default elem;
+  };
+
+  template <class SearchArgsTuple, class... Args>
+  inline constexpr decltype(auto) pick_func_wrapper(Args &&...args) {
+    return pick_func<SearchArgsTuple, Args...>{std::forward<Args>(args)...}.elem;
+  }
+
+  template <class SearchArgsTuple, class Tuple, std::size_t... Sequence>
+  inline constexpr decltype(auto) pick_invocable_impl(Tuple &&t, std::index_sequence<Sequence...>) {
+    return pick_func_wrapper<SearchArgsTuple>(
+        std::forward<std::tuple_element_t<Sequence, Tuple>>(std::get<Sequence>(t))...);
+  }
+
+  template <class SearchArgsTuple, class CandidatesTuple, class DefaultFunc>
+  inline constexpr decltype(auto) pick_invocable(CandidatesTuple t, DefaultFunc &&default_func) {
+    return pick_invocable_impl<SearchArgsTuple>(
+        std::tuple_cat(std::forward_as_tuple(default_func), std::forward<CandidatesTuple>(t)),
+        std::make_index_sequence<1 + std::tuple_size_v<CandidatesTuple>>());
+  }
+} // namespace detail
+
+template <class TupleType>
+struct gesture_holder {
+  TupleType elem;
+};
+
+template <class TupleType>
+gesture_holder(TupleType &&) -> gesture_holder<TupleType>;
+
+template <usagi::concepts::ui::viewable ViewType>
+struct gestures {
+  using value_type = typename ViewType::value_type;
+  using mouse_traits = typename usagi::type::mouse_traits<value_type>;
+
+  template <class TupleType>
+  explicit gestures(TupleType t)
+      : on_down_holder{usagi::ui::detail::pick_invocable<
+            std::tuple<typename mouse_traits::on_down_type, ViewType &>>(t, [](auto, auto &) {})},
+        on_drag_holder{usagi::ui::detail::pick_invocable<
+            std::tuple<typename mouse_traits::on_drag_type, ViewType &>>(t, [](auto, auto &) {})},
+        on_up_holder{usagi::ui::detail::pick_invocable<
+            std::tuple<typename mouse_traits::on_up_type, ViewType &>>(t, [](auto, auto &) {})},
+        on_over_holder{usagi::ui::detail::pick_invocable<
+            std::tuple<typename mouse_traits::on_over_type, ViewType &>>(t, [](auto, auto &) {})} {}
+
+  std::function<void(typename mouse_traits::on_down_type, ViewType &)> on_down_holder;
+  std::function<void(typename mouse_traits::on_drag_type, ViewType &)> on_drag_holder;
+  std::function<void(typename mouse_traits::on_up_type, ViewType &)> on_up_holder;
+  std::function<void(typename mouse_traits::on_over_type, ViewType &)> on_over_holder;
+};
+
+template <usagi::concepts::ui::viewable ViewType>
+struct gesture {
+  using value_type = typename ViewType::value_type;
+  using rect_type = typename usagi::geometry::geometry_traits<value_type>::rect_type;
+  using size_type = typename usagi::geometry::geometry_traits<value_type>::size_type;
+  using draw_context_type = typename ViewType::draw_context_type;
+  using mouse_traits = typename usagi::type::mouse_traits<value_type>;
+  using view_type = typename ViewType::view_type;
+
+  template <class TupleType>
+  gesture(ViewType &&v, TupleType &&t) : holder{std::move(v)}, g{std::forward<TupleType>(t)} {}
+
+  void draw(draw_context_type &context) { holder.draw(context); }
+
+  size_type bounds() const { return holder.bounds(); }
+  rect_type frame() const { return holder.frame(); }
+
+  void event(typename mouse_traits::on_down_type mouse) {
+    g.on_down_holder(mouse, holder);
+    holder.event(mouse);
+  }
+
+  void event(typename mouse_traits::on_drag_type mouse) {
+    g.on_drag_holder(mouse, holder);
+    holder.event(mouse);
+  }
+
+  void event(typename mouse_traits::on_up_type mouse) {
+    g.on_up_holder(mouse, holder);
+    holder.event(mouse);
+  }
+
+  void event(typename mouse_traits::on_over_type mouse) {
+    g.on_over_holder(mouse, holder);
+    holder.event(mouse);
+  }
+
+  view_type &add_sub_view(view_type &&sub_view) {
+    return holder.add_sub_view(std::forward<view_type>(sub_view));
+  }
+
+private:
+  ViewType holder;
+  gestures<ViewType> g;
+};
+
+template <usagi::concepts::ui::viewable ViewType, class TupleType>
+inline constexpr decltype(auto) operator|(ViewType &&v, gesture_holder<TupleType> &&wrapped) {
+  return gesture<ViewType>{std::forward<ViewType>(v),
+                           std::forward<gesture_holder<TupleType>>(wrapped).elem};
+}
+
+template <class... FunctionTypes>
+inline constexpr decltype(auto) gestured(FunctionTypes &&...funcs) {
+  return gesture_holder{std::make_tuple(std::forward<FunctionTypes>(funcs)...)};
+}
+} // namespace usagi::ui
---
+++ test/ui/gesture.cpp
@@ -0,0 +1,185 @@
+#include <gtest/gtest.h>
+#include <usagi/ui/gesture.hpp>
+#include <usagi/ui/view.hpp>
+
+// static test
+namespace {
+struct a_tag {};
+struct b_tag {};
+
+template <usagi::utility::arithmetic ValueType>
+struct SpecificView final : usagi::ui::base_view<ValueType, int> {
+  using value_type = ValueType;
+  using draw_context_type = int;
+  using mouse_traits = typename usagi::ui::base_view<ValueType, int>::mouse_traits;
+
+  explicit SpecificView(std::vector<int> &s) : stamp{s} {}
+
+  void event(typename mouse_traits::on_down_type) override {
+    // 1
+    stamp.emplace_back(1);
+  }
+
+  void event(typename mouse_traits::on_drag_type) override {
+    // 2
+    stamp.emplace_back(2);
+  }
+
+  using usagi::ui::base_view<ValueType, int>::event;
+
+private:
+  std::vector<int> &stamp;
+};
+
+namespace detail {
+  using target_tuple = std::tuple<a_tag, b_tag>;
+
+  // is_apply_invocable
+  static_assert(
+      !usagi::ui::detail::is_apply_invocable<std::function<void(a_tag, a_tag)>, target_tuple>(
+          std::make_index_sequence<std::tuple_size_v<target_tuple>>()));
+  static_assert(
+      usagi::ui::detail::is_apply_invocable<std::function<void(a_tag, b_tag)>, target_tuple>(
+          std::make_index_sequence<std::tuple_size_v<target_tuple>>()));
+} // namespace detail
+
+} // namespace
+
+TEST(GestureTest, IsApplyInvocable) {
+  using target = std::tuple<a_tag, b_tag>;
+  {
+    auto l = [](a_tag, b_tag) {};
+    ASSERT_TRUE((usagi::ui::detail::is_apply_invocable<decltype(l), target>(
+        std::make_index_sequence<std::tuple_size_v<target>>())));
+  }
+  {
+    auto l = [](a_tag, a_tag) {};
+    ASSERT_FALSE((usagi::ui::detail::is_apply_invocable<decltype(l), target>(
+        std::make_index_sequence<std::tuple_size_v<target>>())));
+  }
+  {
+    auto l = [](a_tag, auto) {};
+    ASSERT_TRUE((usagi::ui::detail::is_apply_invocable<decltype(l), target>(
+        std::make_index_sequence<std::tuple_size_v<target>>())));
+  }
+}
+
+TEST(GestureTest, PickFuncWrapper) {
+  // default
+  {
+    auto x = usagi::ui::detail::pick_func_wrapper<std::tuple<a_tag, b_tag>>(
+        // default
+        [](auto, auto) { return true; },
+        // candidates
+        [](a_tag, a_tag) { return false; }, [](b_tag, a_tag) { return false; },
+        [](b_tag, b_tag) { return false; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+
+  // specialized
+  {
+    auto x = usagi::ui::detail::pick_func_wrapper<std::tuple<a_tag, b_tag>>(
+        // default
+        [](auto, auto) { return false; },
+        // candidates
+        [](a_tag, a_tag) { return false; }, [](b_tag, a_tag) { return false; },
+        [](b_tag, b_tag) { return false; }, [](a_tag, b_tag) { return true; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+
+  // auto
+  {
+    auto x = usagi::ui::detail::pick_func_wrapper<std::tuple<a_tag, b_tag>>(
+        // default
+        [](auto, auto) { return false; },
+        // candidates
+        [](a_tag, a_tag) { return false; }, [](a_tag, auto) { return true; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+}
+
+TEST(GestureTest, PickInvocable) {
+  // default
+  {
+    auto x = usagi::ui::detail::pick_invocable<std::tuple<a_tag, b_tag>>(
+        // candidates
+        std::make_tuple([](a_tag, a_tag) { return false; }, [](b_tag, a_tag) { return false; },
+                        [](b_tag, b_tag) { return false; }),
+        // default
+        [](auto, auto) { return true; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+
+  // specialized
+  {
+    auto x = usagi::ui::detail::pick_invocable<std::tuple<a_tag, b_tag>>(
+        // candidates
+        std::make_tuple([](a_tag, a_tag) { return false; }, [](b_tag, a_tag) { return false; },
+                        [](b_tag, b_tag) { return false; }, [](a_tag, b_tag) { return true; }),
+        // default
+        [](auto, auto) { return false; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+
+  // auto
+  {
+    auto x = usagi::ui::detail::pick_invocable<std::tuple<a_tag, b_tag>>(
+        // candidates
+        std::make_tuple([](a_tag, a_tag) { return true; }, [](a_tag, auto) { return true; }),
+        // default
+        [](auto, auto) { return true; })(a_tag{}, b_tag{});
+    ASSERT_TRUE(x);
+  }
+}
+
+TEST(GestureTest, Gestures) {
+  using view = SpecificView<float>;
+
+  std::vector<int> stamp;
+  view v{stamp};
+  usagi::ui::gestures<view> g{std::make_tuple(
+      [&stamp](view::mouse_traits::on_drag_type, auto &) { stamp.emplace_back(0); })};
+
+  {
+    g.on_down_holder(view::mouse_traits::on_down_type{}, v);
+
+    ASSERT_EQ(stamp.size(), 0);
+  }
+
+  stamp.clear();
+
+  {
+    g.on_drag_holder(view::mouse_traits::on_drag_type{}, v);
+
+    // 特殊化した関数が実行されている
+    ASSERT_EQ(stamp.size(), 1);
+    ASSERT_EQ(stamp[0], 0);
+  }
+}
+
+TEST(GestureTest, Gesture) {
+  using view = SpecificView<float>;
+
+  std::vector<int> stamp;
+  auto v = view{stamp} | usagi::ui::gestured(
+                    [&stamp](view::mouse_traits::on_drag_type, auto &) { stamp.emplace_back(0); });
+
+  {
+    v.event(view::mouse_traits::on_down_type{});
+
+    // SpecificView<float>の中身だけ実行されている
+    ASSERT_EQ(stamp.size(), 1);
+    ASSERT_EQ(stamp[0], 1);
+  }
+
+  stamp.clear();
+
+  {
+    v.event(view::mouse_traits::on_drag_type{});
+
+    // 特殊化した関数が先に実行されている
+    ASSERT_EQ(stamp.size(), 2);
+    ASSERT_EQ(stamp[0], 0);
+    ASSERT_EQ(stamp[1], 2);
+  }
+}

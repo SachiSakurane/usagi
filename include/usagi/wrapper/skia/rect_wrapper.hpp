790376962b41148e44542fbb04190d61b998ef50
SachiSakurane <sakuranesachi@gmail.com>
2021-08-29 22:22:16 +0900

    to copy

--- include/usagi/wrapper/skia/rect_wrapper.hpp
+++ include/usagi/wrapper/skia/rect_wrapper.hpp
@@ -5,18 +5,18 @@
 namespace usagi::wrapper::skia {
 
 template <usagi::concepts::geometry::size_concept SizeType>
-SkRect to_rect(SizeType &&size) {
+SkRect to_rect(const SizeType &size) {
   return SkRect::MakeWH(size.width(), size.height());
 }
 
 template <usagi::concepts::geometry::point_concept PointType,
           usagi::concepts::geometry::size_concept SizeType>
-SkRect to_rect(PointType &&point, SizeType &&size) {
+SkRect to_rect(const PointType &point, const SizeType &size) {
   return SkRect::MakeXYWH(point.x(), point.y(), size.width(), size.height());
 }
 
 template <usagi::concepts::geometry::rect_concept RectType>
-SkRect to_rect(RectType &&rect) {
+SkRect to_rect(const RectType &rect) {
   auto [l, t, r, b] = std::forward<RectType>(rect)();
   return SkRect::MakeLTRB(l, t, r, b);
 }

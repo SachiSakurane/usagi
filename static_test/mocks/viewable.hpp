429a59c38065be2985405e097803e311fb57be73
SachiSakurane <sakuranesachi@gmail.com>
2021-11-21 02:36:41 +0900

    fix static_tests

--- static_test/mocks/viewable.hpp
+++ static_test/mocks/viewable.hpp
@@ -18,17 +18,8 @@ struct Viewable : Gestural<ValueType>, Drawable<ValueType, DrawContext> {
   using gesture_parameter_type = typename Gestural<ValueType>::gesture_parameter_type;
   using view_type = usagi::ui::view<value_type, draw_context_type, gesture_parameter_type>;
 
-  using children_type = std::map<view_type, size_t>;
-  using children_key_type = typename children_type::key_type;
-  using children_mapped_type = typename children_type::mapped_type;
-  using children_value_type = typename children_type::value_type;
-
   size_type bounds() const;
   rect_type frame() const;
-  children_value_type &add_sub_view(children_mapped_type &&);
-  children_mapped_type &get_sub_view(children_key_type);
-  bool remove_sub_view(children_key_type);
-  size_t sub_view_size() const;
   void set_enabled(bool flag);
   bool is_enabled() const;
 };

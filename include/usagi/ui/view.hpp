f5e815937692e4a79d59d332398a0c556b3fc9f1
SachiSakurane <sakuranesachi@gmail.com>
2021-11-21 14:05:29 +0900

    fix holder

--- include/usagi/ui/view.hpp
+++ include/usagi/ui/view.hpp
@@ -6,59 +6,62 @@
 #include <usagi/ui/base_view.hpp>
 
 namespace usagi::ui {
-template <usagi::concepts::ui::viewable ViewType>
-class view_holder final : public usagi::ui::base_view<typename ViewType::value_type,
-                                                      typename ViewType::draw_context_type,
-                                                      typename ViewType::gesture_parameter_type> {
-  using base_view_type = typename usagi::ui::base_view<typename ViewType::value_type,
-                                                       typename ViewType::draw_context_type,
-                                                       typename ViewType::gesture_parameter_type>;
+namespace detail {
+  template <usagi::concepts::ui::viewable ViewType>
+  class view_holder final : public usagi::ui::base_view<typename ViewType::value_type,
+                                                        typename ViewType::draw_context_type,
+                                                        typename ViewType::gesture_parameter_type> {
+    using base_view_type = typename usagi::ui::base_view<typename ViewType::value_type,
+                                                         typename ViewType::draw_context_type,
+                                                         typename ViewType::gesture_parameter_type>;
 
-public:
-  using rect_type = typename base_view_type::rect_type;
-  using size_type = typename base_view_type::size_type;
-  using draw_context_type = typename base_view_type::draw_context_type;
-  using gesture_parameter_type = typename base_view_type::gesture_parameter_type;
-  using gesture_traits = typename base_view_type::gesture_traits;
+  public:
+    using rect_type = typename base_view_type::rect_type;
+    using size_type = typename base_view_type::size_type;
+    using draw_context_type = typename base_view_type::draw_context_type;
+    using gesture_parameter_type = typename base_view_type::gesture_parameter_type;
+    using gesture_traits = typename base_view_type::gesture_traits;
 
-  template <class... Args>
-  explicit view_holder(Args &&...args) : holder{std::forward<Args>(args)...} {}
+    view_holder() = default;
 
-  constexpr view_holder(view_holder &&) noexcept = default;
-  view_holder &operator=(view_holder &&) noexcept = default;
+    template <class... Args>
+    constexpr explicit view_holder(Args &&...args) : holder{std::forward<Args>(args)...} {}
 
-  void draw(draw_context_type &d) override { holder.draw(d); }
+    void draw(draw_context_type &d) override { holder.draw(d); }
 
-  size_type bounds() const override { return holder.bounds(); }
-  rect_type frame() const override { return holder.frame(); }
+    size_type bounds() const override { return holder.bounds(); }
+    rect_type frame() const override { return holder.frame(); }
 
-  bool event(typename gesture_traits::on_down_type parameter) override {
-    return holder.event(parameter);
-  }
-  void event(typename gesture_traits::on_drag_type parameter) override { holder.event(parameter); }
-  void event(typename gesture_traits::on_up_type parameter) override { holder.event(parameter); }
-  bool event(typename gesture_traits::on_over_type parameter) override {
-    return holder.event(parameter);
-  }
-  void event(typename gesture_traits::on_out_type parameter) override { holder.event(parameter); }
-  bool event(typename gesture_traits::on_double_type parameter) override {
-    return holder.event(parameter);
-  }
-  bool event(typename gesture_traits::on_wheel_type parameter) override {
-    return holder.event(parameter);
-  }
+    bool event(typename gesture_traits::on_down_type parameter) override {
+      return holder.event(parameter);
+    }
+    void event(typename gesture_traits::on_drag_type parameter) override {
+      holder.event(parameter);
+    }
+    void event(typename gesture_traits::on_up_type parameter) override { holder.event(parameter); }
+    bool event(typename gesture_traits::on_over_type parameter) override {
+      return holder.event(parameter);
+    }
+    void event(typename gesture_traits::on_out_type parameter) override { holder.event(parameter); }
+    bool event(typename gesture_traits::on_double_type parameter) override {
+      return holder.event(parameter);
+    }
+    bool event(typename gesture_traits::on_wheel_type parameter) override {
+      return holder.event(parameter);
+    }
 
-  void set_down(bool flag) override { holder.set_down(flag); }
-  void set_over(bool flag) override { holder.set_over(flag); }
-  [[nodiscard]] bool on_downed() const override { return holder.on_downed(); }
-  [[nodiscard]] bool on_overed() const override { return holder.on_overed(); }
+    void set_down(bool flag) override { holder.set_down(flag); }
+    void set_over(bool flag) override { holder.set_over(flag); }
+    [[nodiscard]] bool on_downed() const override { return holder.on_downed(); }
+    [[nodiscard]] bool on_overed() const override { return holder.on_overed(); }
 
-  void set_enabled(bool flag) override { holder.set_enabled(flag); }
-  [[nodiscard]] bool is_enabled() const override { return holder.is_enabled(); }
+    void set_enabled(bool flag) override { holder.set_enabled(flag); }
+    [[nodiscard]] bool is_enabled() const override { return holder.is_enabled(); }
 
-private:
-  ViewType holder;
-};
+  private:
+    ViewType holder;
+  };
+} // namespace detail
 
 /**
  * viewable を格納する型
@@ -122,6 +125,6 @@ template <usagi::concepts::ui::viewable ViewType, class... Args>
 inline decltype(auto) make_view(Args &&...args) {
   return usagi::ui::view<typename ViewType::value_type, typename ViewType::draw_context_type,
                          typename ViewType::gesture_parameter_type>{
-      std::make_unique<view_holder<ViewType>>(std::forward<Args>(args)...)};
+      std::make_unique<detail::view_holder<ViewType>>(std::forward<Args>(args)...)};
 }
 } // namespace usagi::ui
---
+++ static_test/ui/view.cpp
@@ -0,0 +1,31 @@
+#include <string>
+
+#include <usagi/concepts/ui/viewable.hpp>
+#include <usagi/ui/base_view.hpp>
+#include <usagi/ui/view.hpp>
+
+namespace {
+struct DrawContext {};
+using GestureParameterType = usagi::type::gesture_default_parameter<float>;
+
+static_assert(
+    usagi::concepts::ui::viewable<usagi::ui::view<float, DrawContext, GestureParameterType>>);
+
+namespace view_holder_ {
+  using BaseView = usagi::ui::base_view<float, DrawContext, GestureParameterType>;
+  struct SpecificView : BaseView {
+    constexpr SpecificView(int, std::nullptr_t) : BaseView{} {}
+  };
+
+  static_assert(usagi::concepts::ui::viewable<usagi::ui::detail::view_holder<BaseView>>);
+  static_assert(usagi::concepts::ui::viewable<usagi::ui::detail::view_holder<SpecificView>>);
+  static_assert([]() consteval {
+    usagi::ui::detail::view_holder<BaseView> default_construct{};
+    usagi::ui::detail::view_holder<BaseView> copy_construct{default_construct};
+    usagi::ui::detail::view_holder<BaseView> move_construct{std::move(default_construct)};
+
+    usagi::ui::detail::view_holder<SpecificView> specific_construct{0, nullptr};
+    return true;
+  }());
+} // namespace view_holder_
+} // namespace
